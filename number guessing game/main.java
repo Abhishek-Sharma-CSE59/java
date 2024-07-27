bb0328bf171dd05a10a58bfe35b2d6d2b0876425
Abhishek-Sharma-CSE59 <abhihseksharma.cse59@gmail.com>
2024-07-28 01:49:42 +0530

    added the main file of the game

--- number guessing game/main.java
+++ number guessing game/main.java
@@ -1,4 +1,51 @@
-import java.util.random.*;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class main {
-    int guesses = 0;
+
+    public static void main(String[] args) {
+        int guesses = 0;
+        System.out.println("Type a number :");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int limit = 1;
+        try {
+            limit = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Try Entering integar next time");
+            System.exit(0);
+        }
+        if(limit <= 0){
+            System.out.println("please Enter number greater than 0");
+            System.exit(0);
+        }
+        int comp = random.nextInt(limit);
+        while (true) { 
+            guesses ++;
+            int user_guesses = 0;
+            try {
+                System.out.print("Enter your Guess :");
+                user_guesses = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Try Entering integar next time");
+                continue;
+            }
+            if (user_guesses == comp){
+                System.out.println("You got Right!");
+                break;
+            }
+            else{
+                if(user_guesses < comp){
+                    System.out.println("Your guess is smaller than the number!");
+                }
+                else if(user_guesses > comp){
+                    System.out.println("Your guess is larger then the number");
+                }
+                continue;
+            }
+        }
+    System.out.println("You got this in " + guesses + " attempts");
+    }
+}
     
